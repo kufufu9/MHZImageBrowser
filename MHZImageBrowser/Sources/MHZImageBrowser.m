f0f24c2fb23aa41d06e0ffbdcba3aa61694f0b11
慕徊 <ruihuan.zrh@alibaba-inc.com>
2016-04-17 21:10:55 +0800

    adjust images' position to center

--- MHZImageBrowser/Sources/MHZImageBrowser.m
+++ MHZImageBrowser/Sources/MHZImageBrowser.m
@@ -7,10 +7,11 @@
 //
 #define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
 #define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
-
+#define IMAGE_TAG 3469
 #import "MHZImageBrowser.h"
 #import <objc/runtime.h>
 
+extern NSString *const kScrollViewDelegateKey = @"kScrollViewDelegateKey";
 @interface MHZImageBrowser()<UIScrollViewDelegate>
 @property (nonatomic, strong) UIScrollView *scrollView;
 @property (nonatomic, strong) UIPageControl *pageControl;
@@ -21,10 +22,27 @@
     CGFloat contentWidth = SCREEN_WIDTH * images.count;
     self.scrollView.contentSize = CGSizeMake(contentWidth, SCREEN_HEIGHT);
     [images enumerateObjectsUsingBlock:^(UIImage * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(idx * SCREEN_WIDTH, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+        UIScrollView *imageScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(idx * SCREEN_WIDTH, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+        imageScrollView.maximumZoomScale = 3.f;
+        imageScrollView.delegate = self;
+        imageScrollView.showsVerticalScrollIndicator = NO;
+        imageScrollView.showsHorizontalScrollIndicator = NO;
+        
+        UIImageView *imageView = [[UIImageView alloc] initWithImage:obj];
         imageView.contentMode = UIViewContentModeScaleAspectFit;
-        imageView.image = obj;
-        [self.scrollView addSubview:imageView];
+        imageView.tag = IMAGE_TAG;
+        
+        CGRect imageFrame = imageView.frame;
+        CGFloat width = CGRectGetWidth(self.scrollView.frame);
+        CGFloat p = imageFrame.size.width / width;
+        CGFloat height = CGRectGetHeight(imageFrame) / p;
+        imageView.frame = CGRectMake(0, 0, width, height);
+        
+        imageView.center = self.scrollView.center;
+        imageScrollView.scrollsToTop = YES;
+        imageScrollView.contentSize = CGSizeMake(width, height);
+        [imageScrollView addSubview:imageView];
+        [self.scrollView addSubview:imageScrollView];
     }];
     
     UIView *containerView = [UIApplication sharedApplication].keyWindow;
@@ -35,15 +53,37 @@
     [containerView addSubview:self.pageControl];
     
     NSLayoutConstraint *pageControlCenterXconstraint = [NSLayoutConstraint constraintWithItem:self.pageControl attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.pageControl.superview attribute:NSLayoutAttributeCenterX multiplier:1.f constant:0.f];
-    NSLayoutConstraint *pageControlYconstraint = [NSLayoutConstraint constraintWithItem:self.pageControl attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.pageControl.superview attribute:NSLayoutAttributeBottom multiplier:1.f constant:-50.f];
+    NSLayoutConstraint *pageControlYconstraint = [NSLayoutConstraint constraintWithItem:self.pageControl attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.pageControl.superview attribute:NSLayoutAttributeBottom multiplier:1.f constant:-30.f];
     [containerView addConstraints:@[pageControlCenterXconstraint, pageControlYconstraint]];
 }
 
 #pragma mark UIScrollViewDelegate
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (scrollView != self.scrollView) {
+        return;
+    }
     NSInteger currentIndex = scrollView.contentOffset.x / SCREEN_WIDTH;
     self.pageControl.currentPage = currentIndex;
 }
+
+#pragma mark UIScrollViewDelegate-Zoom
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    UIView *imageView = [scrollView viewWithTag:IMAGE_TAG];
+    return imageView;
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView {
+    CGFloat offsetX = 0.0, offsetY = 0.0;
+    if (scrollView.bounds.size.width > scrollView.contentSize.width) {
+        offsetX = (scrollView.bounds.size.width - scrollView.contentSize.width) * 0.5;
+    }
+    if (scrollView.bounds.size.height > scrollView.contentSize.height) {
+        offsetY = (scrollView.bounds.size.height - scrollView.contentSize.height) * 0.5;
+    }
+    [self viewForZoomingInScrollView:scrollView].center = CGPointMake(scrollView.contentSize.width * 0.5 + offsetX, scrollView.contentSize.height * 0.5 + offsetY);
+//    [self viewForZoomingInScrollView:scrollView].center = self.scrollView.center;
+}
+
 #pragma mark getter & setter
 - (UIScrollView *)scrollView {
     if (!_scrollView) {
@@ -51,7 +91,7 @@
         _scrollView.backgroundColor = [UIColor blackColor];
         _scrollView.pagingEnabled = YES;
         _scrollView.delegate = self;
-        objc_setAssociatedObject(_scrollView, @"1", self, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        objc_setAssociatedObject(_scrollView, kScrollViewDelegateKey, self, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
     return _scrollView;
 }
